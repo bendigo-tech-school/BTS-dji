e1773f345c587bf12333cfc142dd7fdffaeb59c3
Bevan matsacos <bmsomething98@gmail.com>
2023-05-09 13:07:06 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 # This call to setup() does all the work
 setup(
     name="BTS_dji",
-    version="0.1.1",
+    version="0.1.2",
     description="A simplified version of the dji robomaster library with some addons. Made for the bendigo tech school.",
     long_description=long_description,
     long_description_content_type="text/markdown",
